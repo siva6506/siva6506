4953c97ca9189e923b9a3040dbd9c49256582c24
siva6506 <kumargovri0@gmail.com>
2025-12-31 15:39:38 +0530

    we are done

---
+++ kumar/project 1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>  // This line includes the standard input-output library needed for printf
+
+// main() is the entry point of every C program
+int main() {
+    // Print your name on the first line
+    printf("Name: CSECS\n");  
+    
+    // Print your age on the second line
+    printf("Age: 21\n");      
+    
+    // Print your country on the third line
+    printf("Country: India\n"); 
+    
+    // Return 0 to indicate successful program execution
+    return 0;                 
+}
